c6bac80e70bcc3627e9f9ef74af8c12cb03fac8b
Mesekir19 <mesekirsalgech@example.com>
2023-02-24 22:42:58 +0300

    commit

--- 0x05-Node_JS_basic/0-console.js
+++ 0x05-Node_JS_basic/0-console.js
@@ -1,2 +1,10 @@
-const displayMessage = (aString) => console.log(aString);
+/**
+ * A function which dispaly a message to the STDOUT
+ * @param {String} message the message to be display
+ * @author Yidnekachew Bantrga <https://github.com/Yidne21>
+ */
+const displayMessage = (message) => {
+  console.log(message);
+};
+
 module.exports = displayMessage;
--- 0x05-Node_JS_basic/4-http.js
+++ 0x05-Node_JS_basic/4-http.js
@@ -1,11 +1,20 @@
-const http = require('http');
+const http = require("http");
 
-const port = 1245;
+const PORT = 1245;
+const HOST = "localhost";
+const app = http.createServer();
 
-const app = http.createServer((req, res) => {
+app.on("request", (_, res) => {
+  const responseText = "Hello Holberton School!";
+
+  res.setHeader("Content-Type", "text/plain");
+  res.setHeader("Content-Length", responseText.length);
   res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello Holberton School!');
+  res.write(Buffer.from(responseText));
 });
-app.listen(port);
+
+app.listen(PORT, HOST, () => {
+  process.stdout.write(`Server listening at -> http://${HOST}:${PORT}\n`);
+});
+
 module.exports = app;
